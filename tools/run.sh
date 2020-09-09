41dd37d0a501b58b15efd9c96e8a404787bef0f1
heartcored98 <33013451+heartcored98@users.noreply.github.com>
2020-09-09 21:58:37 +0900

    Add project config and UI config for car warning detection

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-label-studio start ${PROJECT_NAME:-my_project} --init -b --host 0.0.0.0 --port ${PORT:-8080}
+label-studio start ${PROJECT_NAME:-car_warning_project} --init -b --host 0.0.0.0 --port 8082 \
+--username dingbro --password qwer1234 \
+--source s3 --source-path car-warning-dataset \
+--source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images1\", \"regex\": \"^.*.(jpeg|jpg|JPG|JPEG)\" }" \
+--target s3-completions --target-path car-warning-dataset \
+--target-params "{\"prefix\": \"outputs1\", \"create_local_copy\": true}" \
+-l config.xml -c project_config.json
+--sampling uniform \
+--log-level ERROR \
+--force
