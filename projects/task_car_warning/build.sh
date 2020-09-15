df5ea2f9536dce3ca46fd9688c0e2857765b534a
heartcored98 <kevin.jo@dingbro.ai>
2020-09-16 02:19:12 +0900

    Update build.sh for gcr

--- projects/task_car_warning/build.sh
+++ projects/task_car_warning/build.sh
@@ -1,7 +1,10 @@
 cd ../../
 
 export PROJECT_NAME=task_car_warning
-export PORT=10000
+export PROJECT_ID=doctorcha-ai
+export HOSTNAME=asia.gcr.io
+
+export PORT=5000
 export GOOGLE_APPLICATION_CREDENTIALS="doctorcha-ai-3a236e79d498.json"
 
 mkdir temp
@@ -10,7 +13,7 @@ cp ./projects/${PROJECT_NAME}/config.xml temp/config.xml
 cp ./projects/${PROJECT_NAME}/project_config.json temp/project_config.json
 cp ./.credential/${GOOGLE_APPLICATION_CREDENTIALS} temp/${GOOGLE_APPLICATION_CREDENTIALS}
 
-docker-compose up --build -d
+docker-compose up -d # --build
 
 echo "\n========================================"
 echo "Server running at http://0.0.0.0:${PORT}"
