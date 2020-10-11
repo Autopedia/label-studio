17a41869cdbda432b5cbea088363ea2524686c9a
heartcored98 <kevin.jo@dingbro.ai>
2020-10-11 16:51:23 +0900

    Fix item_name width with max pixel and update task to json dir

---
+++ projects/task_text_header/docker-entrypoint2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS="doctorcha-ai-3a236e79d498.json"
+
+export PROJECT_NAME=task_text_header
+export PORT=7000
+
+label-studio start ${PROJECT_NAME} --init -b --host 0.0.0.0 --port ${PORT} \
+--ml-backend http://localhost:9090 \
+--username dingbro --password qwer1234 \
+-l config.xml \
+--source gcs --source-path text-header-dataset \
+--source-params "{\"use_blob_urls\": false, \"prefix\": \"tasks_resize\", \"regex\": \"^.*.json\" }" \
+-c project_config.json \
+--sampling uniform \
+--log-level INFO \
+--force \
+--target gcs-completions --target-path text-header-dataset \
+--target-params "{\"prefix\": \"outputs_resize\"}" \
+# --use-gevent
+
+
+
+# --input-path=tasks \ 
+# --input-format=json-dir \
+
+# --source gcs --source-path text-header-dataset \
+# --source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"images1\", \"regex\": \"^.*.(jpeg|jpg|JPG|JPEG)\" }" \
