542898c487d4510342985449837952ba78a9a265
heartcored98 <kevin.jo@dingbro.ai>
2020-10-22 20:21:45 +0900

    Add ah, bgij, e xray projects

--- projects/task_tire_xray/e-docker-entrypoint.sh
+++ projects/task_tire_xray/e-docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS="../../.credential/dingbro-aichampionship.json"
-export PROJECT_NAME=task_tire_xray_ah
-export PORT=8001
+export PROJECT_NAME=task_tire_xray_e
+export PORT=8003
 
 label-studio start ${PROJECT_NAME} --init -b --host 0.0.0.0 --port ${PORT} \
 --username dingbro --password qwer1234 \
--l ah-config.xml \
+-l e-config.xml \
 -c project_config.json \
---sampling sequential \
+--sampling uniform \
 --log-level INFO \
 --force \
 --source gcs --source-path hankooktier \
---source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"data4ls_ah_images_2\", \"regex\": \"^.*.(jpeg|jpg|JPG|JPEG)\" }" \
+--source-params "{\"use_blob_urls\": true, \"data_key\": \"image\", \"prefix\": \"data4ls_e_images\", \"regex\": \"^.*.(jpeg|jpg|JPG|JPEG)\" }" \
 --target gcs-completions --target-path hankooktier \
---target-params "{\"prefix\": \"data4ls_ah_outputs_2\", \"create_local_copy\": false}" \
+--target-params "{\"prefix\": \"data4ls_e_outputs\", \"create_local_copy\": false}" \
 --use-gevent
